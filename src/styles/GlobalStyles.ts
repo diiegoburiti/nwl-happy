0efa6a2bc3844a513dd7822c221282cb9d9b9ecb
Diego Buriti <bs.diego97@gmail.com>
2020-10-13 09:04:15 -0300

    Global styles.

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  color: #fff;
+  background: #feb2f5;
+}
+
+body, input, textarea, button {
+  font: 600 18px  'Nunito', sans-serif;
+}
+`;
