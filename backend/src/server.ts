dbbb0e412bc33408717e22eccfd3f0d5a274ab01
Diego Buriti <bs.diego97@gmail.com>
2020-10-13 18:04:56 -0300

    Added config typeOrm.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,12 @@
-import express, { response } from "express";
+import express from "express";
+import "./database/connection";
 
 const app = express();
+app.use(express.json());
 
-app.get("/", (request, response) => {
+app.post("/orphanages", (request, response) => {
+  console.log(request.body);
+
   return response.json({ message: "Hello world" });
 });
 app.listen(3333);
