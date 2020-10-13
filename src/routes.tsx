137849d4b0083cd6d5a356257781bff3849e544f
Diego Buriti <bs.diego97@gmail.com>
2020-10-12 23:33:42 -0300

    Initial configs routes.

---
+++ src/routes.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Landing from "./pages/landing";
+import OrphanagesMap from "./pages/orphanagesMap";
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Landing} />
+        <Route path="/app" component={OrphanagesMap} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
