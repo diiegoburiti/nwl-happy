f090e148bb7aee14b1ae538a86e759b67ca43cea
Diego Buriti <bs.diego97@gmail.com>
2020-10-13 15:49:02 -0300

    Initial configs node and typescript.

---
+++ backend/src/server.ts
@@ -0,0 +1,8 @@
+import express, { response } from "express";
+
+const app = express();
+
+app.get("/", (request, response) => {
+  return response.json({ message: "Hello world" });
+});
+app.listen(3333);
