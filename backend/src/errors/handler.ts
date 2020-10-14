5c8adb13f0d104ac5544bd4f51cce4d0a155d5f5
Diego Buriti <bs.diego97@gmail.com>
2020-10-14 17:33:45 -0300

    Added error to return

--- backend/src/errors/handler.ts
+++ backend/src/errors/handler.ts
@@ -6,19 +6,20 @@ interface ValidationErrors {
   [key: string]: string[];
 }
 
-const errorHandler: ErrorRequestHandler = (request, error, response, next) => {
+const errorHandler: ErrorRequestHandler = (error, request, response, next) => {
   if (error instanceof ValidationError) {
     let errors: ValidationErrors = {};
 
     error.inner.forEach((err) => {
       errors[err.path] = err.errors;
     });
-    return response.status(400).json({ message: "Validation fails" });
+
+    return response.status(400).json({ message: "Validation fails", errors });
   }
 
   console.error(error);
 
-  return response.status(500).json({ message: "internal server error" });
+  return response.status(500).json({ message: "Internal server error" });
 };
 
 export default errorHandler;
