6d6a5155d04b90ad4189cc0973ac0e5509dbfdd3
Diego Buriti <bs.diego97@gmail.com>
2020-10-14 17:27:46 -0300

    Validation request

--- backend/src/errors/handler.ts
+++ backend/src/errors/handler.ts
@@ -1,6 +1,21 @@
-import { ErrorRequestHandler, json } from "express";
+import { ErrorRequestHandler } from "express";
+
+import { ValidationError } from "yup";
+
+interface ValidationErrors {
+  [key: string]: string[];
+}
 
 const errorHandler: ErrorRequestHandler = (request, error, response, next) => {
+  if (error instanceof ValidationError) {
+    let errors: ValidationErrors = {};
+
+    error.inner.forEach((err) => {
+      errors[err.path] = err.errors;
+    });
+    return response.status(400).json({ message: "Validation fails" });
+  }
+
   console.error(error);
 
   return response.status(500).json({ message: "internal server error" });
