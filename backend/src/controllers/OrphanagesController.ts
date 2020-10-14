03808aa694a86b1767f71ade8ebfab8245c59e06
Diego Buriti <bs.diego97@gmail.com>
2020-10-14 08:41:21 -0300

    Added some endpoints.

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -4,6 +4,23 @@ import { getRepository } from "typeorm";
 import Orphanage from "../models/Orphanage";
 
 export default {
+  async index(request: Request, response: Response) {
+    const orphanagesRepository = getRepository(Orphanage);
+
+    const orphanages = await orphanagesRepository.find();
+
+    return response.json(orphanages);
+  },
+
+  async show(request: Request, response: Response) {
+    const { id } = request.params;
+    const orphanagesRepository = getRepository(Orphanage);
+
+    const orphanage = await orphanagesRepository.findOneOrFail(id);
+
+    return response.json(orphanage);
+  },
+
   async create(request: Request, response: Response) {
     const {
       name,
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,6 +3,10 @@ import OrphanageController from "./controllers/OrphanagesController";
 
 const routes = Router();
 
+routes.get("/orphanages", OrphanageController.index);
+
+routes.get("/orphanages/:id", OrphanageController.show);
+
 routes.post("/orphanages", OrphanageController.create);
 
 export default routes;
