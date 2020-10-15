bd25f43cec3ac467befb3b75d7b9dfc0d945aef6
Diego Buriti <bs.diego97@gmail.com>
2020-10-15 08:50:33 -0300

    Added createOrphanage page.

---
+++ frontend/src/pages/createOrphanage/CreateOrphanage.tsx
@@ -0,0 +1,120 @@
+import React from "react";
+import { Map, Marker, TileLayer } from "react-leaflet";
+import L from "leaflet";
+import { useHistory } from "react-router-dom";
+
+import { FiArrowLeft, FiPlus } from "react-icons/fi";
+
+import mapMarkerImg from "../../assets/img/map-market.svg";
+
+import {
+  Wrapper,
+  Aside,
+  AsideFooter,
+  MainContent,
+  CreateOrphanageForm,
+  InputBlock,
+  NewImage,
+  ButtonSelect,
+  Active,
+  ConfirmButton,
+} from "./style";
+
+const happyMapIcon = L.icon({
+  iconUrl: mapMarkerImg,
+
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [0, -60],
+});
+
+export default function CreateOrphanage() {
+  const { goBack } = useHistory();
+
+  return (
+    <Wrapper>
+      <Aside>
+        <img src={mapMarkerImg} alt="Happy" />
+
+        <AsideFooter>
+          <button type="button" onClick={goBack}>
+            <FiArrowLeft size={24} color="#FFF" />
+          </button>
+        </AsideFooter>
+      </Aside>
+
+      <MainContent>
+        <CreateOrphanageForm>
+          <fieldset>
+            <legend>Dados</legend>
+
+            <Map
+              center={[-27.2092052, -49.6401092]}
+              style={{ width: "100%", height: 280 }}
+              zoom={15}
+            >
+              <TileLayer
+                url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+              />
+
+              <Marker
+                interactive={false}
+                icon={happyMapIcon}
+                position={[-27.2092052, -49.6401092]}
+              />
+            </Map>
+
+            <InputBlock>
+              <label htmlFor="name">Nome</label>
+              <input id="name" />
+            </InputBlock>
+
+            <InputBlock>
+              <label htmlFor="about">
+                Sobre <span>Máximo de 300 caracteres</span>
+              </label>
+              <textarea id="name" maxLength={300} />
+            </InputBlock>
+
+            <InputBlock>
+              <label htmlFor="images">Fotos</label>
+
+              <div className="uploaded-image"></div>
+
+              <NewImage>
+                <FiPlus size={24} color="#15b6d6" />
+              </NewImage>
+            </InputBlock>
+          </fieldset>
+
+          <fieldset>
+            <legend>Visitação</legend>
+
+            <InputBlock>
+              <label htmlFor="instructions">Instruções</label>
+              <textarea id="instructions" />
+            </InputBlock>
+
+            <InputBlock>
+              <label htmlFor="opening_hours">Nome</label>
+              <input id="opening_hours" />
+            </InputBlock>
+
+            <InputBlock>
+              <label htmlFor="open_on_weekends">Atende fim de semana</label>
+
+              <ButtonSelect>
+                <Active type="button">Sim</Active>
+                <button type="button">Não</button>
+              </ButtonSelect>
+            </InputBlock>
+          </fieldset>
+
+          <ConfirmButton type="submit">Confirmar</ConfirmButton>
+        </CreateOrphanageForm>
+      </MainContent>
+    </Wrapper>
+  );
+}
+
+// return `https://a.tile.openstreetmap.org/${z}/${x}/${y}.png`;
---
+++ frontend/src/pages/createOrphanage/style.ts
@@ -0,0 +1,189 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+`;
+
+export const Aside = styled.aside`
+  position: fixed;
+  height: 100%;
+  padding: 32px 24px;
+  background: linear-gradient(329.54deg, #15b6d6 0%, #15d6d6 100%);
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+
+  img {
+    width: 48px;
+  }
+`;
+
+export const AsideFooter = styled.footer`
+  button,
+  a {
+    width: 48px;
+    height: 48px;
+
+    border: 0;
+
+    background: #12afcb;
+    border-radius: 16px;
+
+    cursor: pointer;
+
+    transition: background-color 0.2s;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    &:hover {
+      background: #17d6eb;
+    }
+  }
+`;
+
+export const MainContent = styled.main`
+  flex: 1;
+`;
+
+export const CreateOrphanageForm = styled.form`
+  width: 700px;
+  margin: 64px auto;
+  background: #ffffff;
+  border: 1px solid #d3e2e5;
+  border-radius: 20px;
+  padding: 64px 80px;
+  overflow: hidden;
+
+  fieldset {
+    border: 0;
+    legend {
+      width: 100%;
+
+      font-size: 32px;
+      line-height: 34px;
+      color: #5c8599;
+      font-weight: 700;
+
+      border-bottom: 1px solid #d3e2e5;
+      margin-bottom: 40px;
+      padding-bottom: 24px;
+    }
+  }
+  fieldset + fieldset {
+    margin-top: 80px;
+  }
+`;
+
+export const InputBlock = styled.div`
+  margin-top: 24px;
+
+  label {
+    display: flex;
+    color: #8fa7b3;
+    margin-bottom: 8px;
+    line-height: 24px;
+
+    span {
+      font-size: 14px;
+      color: #8fa7b3;
+      margin-left: 24px;
+      line-height: 24px;
+    }
+  }
+
+  input,
+  textarea {
+    width: 100%;
+    background: #f5f8fa;
+    border: 1px solid #d3e2e5;
+    border-radius: 20px;
+    outline: none;
+    color: #5c8599;
+  }
+
+  input {
+    height: 64px;
+    padding: 0 16px;
+  }
+
+  textarea {
+    min-height: 120px;
+    max-height: 240px;
+    resize: vertical;
+    padding: 16px;
+    line-height: 28px;
+  }
+`;
+
+export const NewImage = styled.button`
+  width: 100%;
+  height: 64px;
+  background: #f5f8fa;
+  border: 1px dashed #96d2f0;
+  border-radius: 20px;
+  cursor: pointer;
+`;
+
+export const ButtonSelect = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+
+  button {
+    height: 64px;
+    background: #f5f8fa;
+    border: 1px solid #d3e2e5;
+    color: #5c8599;
+    cursor: pointer;
+
+    /*  .active {
+      background: #edfff6;
+      border: 1px solid #a1e9c5;
+      color: #37c77f;
+    } */
+
+    &:first-child {
+      border-radius: 20px 0px 0px 20px;
+    }
+
+    &:last-child {
+      border-radius: 0 20px 20px 0;
+      border-left: 0;
+    }
+  }
+`;
+
+export const Active = styled.button`
+  background: #edfff6;
+  border: 1px solid #a1e9c5;
+  color: #37c77f;
+`;
+
+export const ConfirmButton = styled.button`
+  margin-top: 64px;
+
+  width: 100%;
+  height: 64px;
+  border: 0;
+  cursor: pointer;
+  background: #3cdc8c;
+  border-radius: 20px;
+  color: #ffffff;
+  font-weight: 800;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  transition: background-color 0.2s;
+
+  svg {
+    margin-right: 16px;
+  }
+  &:hover {
+    background: #36cf82;
+  }
+`;
